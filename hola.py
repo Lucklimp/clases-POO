61872cbe820e30e740a08ffb35c7b4414c9906bf
Lucas <lucasmendoza.12.2004@gmail.com>
2024-10-07 20:34:13 -0300

    eliminacion

--- hola.py
+++
@@ -1,6 +0,0 @@
-if(0<1):
-    print("hola")
-elif(0>1):
-    print("chao")
-else:
-    print("chao")
--- if.py
+++
@@ -1,7 +0,0 @@
-if(0<1):
-    print("hola")
-elif(0>1):
-    print("chao")
-else:
-    print("adios")
-    
