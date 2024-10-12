d52f1f4ca90d6cfa085a4d1187f229fd11a2cd85
Lucas <lucasmendoza.12.2004@gmail.com>
2024-10-12 08:22:16 -0300

    cambios

--- biblioteca/clases,py/detalle_libro.py
+++ biblioteca/clases,py/detalle_libro.py
@@ -2,10 +2,11 @@ import libro
 import editorial
 
 class DetalleLibro(libro, editorial):
-    def __init__(self, id_detalle_libro, isbn, id_editorial, numero_paginas, n_copias, edicion):
+    def __init__(self, id_detalle_libro, isbn, id_editorial, numero_paginas, estado, n_copias, edicion):
         libro.__init__(isbn)
         editorial.__init__(id_editorial)
         self.id_detalle_libro = id_detalle_libro
         self.numero_paginas = numero_paginas
+        self.estado = estado
         self.n_copias = n_copias
         self.edicion = edicion
